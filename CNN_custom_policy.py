430ec1f2e5124ba4eeba05b8b70abce177ebd850
rmalone1097 <31319374+rmalone1097@users.noreply.github.com>
2022-11-29 18:55:00 -0500

    Added layers and switched feature dim on CNN

--- CNN_custom_policy.py
+++ CNN_custom_policy.py
@@ -6,14 +6,15 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 class CustomCNN(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Box, features_dim:int=1024):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim:int=256):
         super(CustomCNN, self).__init__(observation_space, features_dim)
         n_input_channels = observation_space.shape[1]
-        #print(type(n_input_channels))
         self.cnn = nn.Sequential(
-            nn.Conv1d(n_input_channels, 64, 1, padding='same'),
-            nn.Tanh(),
-            nn.MaxPool1d(1),
+            nn.Conv1d(n_input_channels, 32, kernel_size=7, padding='same'),
+            nn.ReLU(),
+            nn.Conv1d(32, 64, kernel_size=5, padding='same'),
+            nn.ReLU(),
+            nn.Conv1d(64, 64, kernel_size=3),
             nn.ReLU(),
             nn.Flatten()
         )
