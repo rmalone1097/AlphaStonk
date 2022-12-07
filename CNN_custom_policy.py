c2ba9e515373135fe36142a899c7b28df6e5a903
Robert Malone <rmalone1097@gmail.com>
2022-12-07 10:01:41 -0700

    changed default feature dim to 1024

--- CNN_custom_policy.py
+++ CNN_custom_policy.py
@@ -6,7 +6,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 class CustomCNN(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Box, features_dim:int=256):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim:int=1024):
         super(CustomCNN, self).__init__(observation_space, features_dim)
         n_input_channels = observation_space.shape[1]
         self.cnn = nn.Sequential(
