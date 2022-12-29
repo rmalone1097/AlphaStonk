f8ae8f329d166515b2a5f7462609212e67252b91
rmalone1097 <31319374+rmalone1097@users.noreply.github.com>
2022-12-29 17:26:15 -0500

    implemented os total concat
    Runs all channels through os block, concatenates them, and runs them through another os block

--- omni_scale_CNN.py
+++ omni_scale_CNN.py
@@ -5,21 +5,41 @@ import torch.nn as nn
 from stable_baselines3 import PPO
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
+# List of prime numbers as int
+with open('prime_numbers.txt') as f:
+    lines = f.readlines()
+prime_list = lines[0].split(",")
+prime_list = [int(n) for n in prime_list]
+
 class CustomCombinedExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Dict, features_dim=1024):
+    def __init__(self, observation_space: gym.spaces.Dict, features_dim=1024, prime_list=prime_list):
         # features_dim is specifically for the output dim of the CNN. Will have to be added to output
         # for vector extractor for total featuers_dim, which is self.features_dim later on
         super(CustomCombinedExtractor, self).__init__(observation_space, features_dim)
 
+        n_input_features = observation_space['slice'].shape[0]
         n_input_channels = observation_space['slice'].shape[1]
-        self.os_layer_1 = nn.Sequential(
-            nn.Conv1d(n_input_channels, 32, kernel_size=7, padding='same'),
-            nn.ReLU(),
-            nn.Conv1d(32, 64, kernel_size=5, padding='same'),
-            nn.ReLU(),
-            nn.Conv1d(64, 64, kernel_size=3, padding='same'),
-            nn.ReLU(),
-            nn.Flatten()
+
+        self.kernels_1 = nn.ModuleList()
+        self.kernels_2 = nn.ModuleList(nn.Conv1d(1, 1, kernel_size=1, padding='same'), nn.Conv1d(1, 1, kernel_size=2, padding='same'))
+        conv_1_length = 0
+
+        for p in prime_list:
+            if p >= n_input_features // 2:
+                conv = nn.Conv1d(1, 1, kernel_size=p, padding='same')
+                self.kernels_1.append(conv)
+                conv_1_length += n_input_features - p + 1
+        
+        # Not including convolutions
+        self.os_block_1 = nn.Sequential(
+            nn.BatchNorm1d(conv_1_length),
+            nn.ReLU()
+        )
+
+        conv_2_length = (n_input_features - 1 + 1) + (n_input_features - 2 + 1)
+        self.os_block_2 = nn.Sequential(
+            nn.BatchNorm1d(conv_2_length),
+            nn.ReLU
         )
 
         self.linear = nn.Sequential(
@@ -33,9 +53,7 @@ class CustomCombinedExtractor(BaseFeaturesExtractor):
         # so go over all the spaces and compute output feature sizes
         for key, subspace in observation_space.spaces.items():
             if key == "slice":
-                # We will just downsample one channel of the image by 4x4 and flatten.
-                # Assume the image is single-channel (subspace.shape[0] == 0)
-                extractors[key] = nn.Sequential(self.cnn, self.linear)
+                extractors[key] = nn.Sequential(self.linear)
                 total_concat_size += features_dim
             elif key == "vector":
                 # Run through a simple MLP
@@ -53,7 +71,41 @@ class CustomCombinedExtractor(BaseFeaturesExtractor):
         # self.extractors contain nn.Modules that do all the processing.
         for key, extractor in self.extractors.items():
             if key == 'slice':
+
+                # Batch, length, channel
                 observations['slice']  = torch.permute(observations['slice'], (0, 2, 1))
+                obs_clone = torch.clone(observations['slice'])
+
+                # iterate through channels and implement OS block on all
+                n_channels = obs_clone.size(dim=2)
+                for  i in range (n_channels):
+                    x = [k(obs_clone[:, :, i]) for k in self.kernels_1]
+                    x = torch.cat(x, dim=1)
+                    x = self.os_block_1(x)
+                    x = [k(x) for k in self.kernels_1]
+                    x = torch.cat(x, dim=1)
+                    x = self.os_block_1(x)
+                    x = [k(x) for k in self.kernels_2]
+                    x = torch.cat(x, dim=1)
+                    x = self.os_block_2(x)
+
+                    # os_out should have shape [batch, length]
+                    if not os_out:
+                        os_out = x
+                    else:
+                        os_out = torch.cat((os_out, x), dim=1)
+                
+                # Take concatenated output from previous os block and run it though a new os block
+                x = [k(os_out) for k in self.kernels_1]
+                x = torch.cat(x, dim=1)
+                x = self.os_block_1(x)
+                x = [k(x) for k in self.kernels_1]
+                x = torch.cat(x, dim=1)
+                x = self.os_block_1(x)
+                x = [k(x) for k in self.kernels_2]
+                x = torch.cat(x, dim=1)
+                x = self.os_block_2(x)
+
             encoded_tensor_list.append(extractor(observations[key]))
         # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
         return torch.cat(encoded_tensor_list, dim=1)
