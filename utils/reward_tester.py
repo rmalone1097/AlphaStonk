f4db2efcaef856c408ccd826ee7337fd297fbb6c
Robert Malone <rmalone1097@gmail.com>
2023-01-25 17:48:25 -0700

    added action and price list params

--- utils/reward_tester.py
+++ utils/reward_tester.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import mpmath
 
-# The higher the decay factor, the less aggressively time is penalized. Decay factor = time at which a positive net trade will generate a negative reward.
-
 class RewardModel():
-    def __init__(self, price_list:list) -> None:
+    def __init__(self, action:int, price_list:list) -> None:
         self.price_list = price_list
-        self.reward_lists = []
+        self.action = action
+        self.reward_list = []
 
     def decayOne(self, decay_factor, minimum_roi):
         for net in self.net_list:
@@ -18,18 +17,12 @@ class RewardModel():
                     reward = -net * (mpmath.acot(time - decay_factor) - mpmath.pi / 2)
                 reward_list.append(reward)
 
-            self.reward_lists.append(reward_list)
-        print(self.reward_lists)
     
     def plotRewards(self):
         fig, ax = plt.subplots()
-        for i, reward_list in enumerate(self.reward_lists):
+        for i, reward_list in enumerate(self.reward_list):
             ax.plot(self.times_list, reward_list, label='Net: ' + str(self.net_list[i]))
         legend = ax.legend(loc='upper right', shadow=True, fontsize='large')
         plt.grid()
 
         plt.show()
-    
-new = RewardModel([5, 10, 30, 50, 75, 100, 125, 150, 200, 300, 390, 400, 500], [-0.1,-0.05, 0.01, 0.05, 0.1, 0.15, 0.20, 0.5])
-new.decayOne(390, 0)
-new.plotRewards()
