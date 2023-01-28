fb324e8a4250c844e56699b7673a0b9773763c91
Robert Malone <rmalone1097@gmail.com>
2023-01-28 10:56:12 -0700

    moved commented data builder to new file

---
+++ utils/data_builder.py
@@ -0,0 +1,8 @@
+#fetch_all_data('SPY', 1, '2012-08-22', '2022-08-22')
+#file_path = write_data_to_new_file('AAPL', 1, '2022-05-19', '2022-05-20')
+'''df = df_builder('SPY_minute_2012-08-22.csv')
+filepath = os.getcwd() + '\\SPY_minute_2012-08-22_built.pkl'
+df.to_pickle(filepath)'''
+#df_slice = df.iloc[0:100]
+#array = df_slice.loc[:,'open':].to_numpy()
+#print(datetime.datetime.fromtimestamp(df.iloc[0].name / 1000))
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -235,12 +235,3 @@ def plot_energy_cloud(df, starting_index=0, ending_index=30):
         mpf.make_addplot(df_slice['zero_reward'], panel=2, ylabel='Zero Reward'),
         mpf.make_addplot(df_slice['energy'], panel=3, color='orange', ylabel='Energy')]
     mpf.plot(df.iloc[starting_index:ending_index], type='candle', addplot=taplots)
-
-#fetch_all_data('SPY', 1, '2012-08-22', '2022-08-22')
-#file_path = write_data_to_new_file('AAPL', 1, '2022-05-19', '2022-05-20')
-'''df = df_builder('SPY_minute_2012-08-22.csv')
-filepath = os.getcwd() + '\\SPY_minute_2012-08-22_built.pkl'
-df.to_pickle(filepath)'''
-#df_slice = df.iloc[0:100]
-#array = df_slice.loc[:,'open':].to_numpy()
-#print(datetime.datetime.fromtimestamp(df.iloc[0].name / 1000))
