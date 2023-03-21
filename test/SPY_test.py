8f6d5410f3f641b24cea1eeb928df7f30a494454
rmalone1097 <31319374+rmalone1097@users.noreply.github.com>
2023-03-21 14:03:37 -0400

    added a read csv function to SPY test

--- test/SPY_test.py
+++ test/SPY_test.py
@@ -3,7 +3,7 @@ import os
 import csv
 import pandas as pd
 import numpy as np
-import gym
+import gymnasium as gym
 import matplotlib.pyplot as plt
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -14,11 +14,11 @@ from pathlib import Path
 from envs.stock_env_test import StockEnv
 from ray.rllib.algorithms.algorithm import Algorithm
 
-tickers = ['SPY', 'AAPL', 'BAC']
+tickers = ['SPY']
 cande_length = 1
 start_stamp = 928761600
 end_stamp = 1676581140
-data_path = Path.home() / 'data'
+data_path = Path.home() / 'Documents' / 'Datasets'
 _, _, full_test_df, obs_test_df = prepare_state_df(tickers, data_path, 2206200)
 
 short_df = pd.read_pickle(Path.home() / 'data' / 'SPY_minute_2022-08-22_built.pkl')
@@ -44,40 +44,33 @@ def test_algo(algo_path, env, roi_file_name, portfolio_file_name):
         roi_list.append(env.total_roi)
     return roi_list
 
-with open(Path.home() / 'Git' /  'AlphaStonk' / 'test' / 'SPY_AAPL_BAC_PPO_results.csv', newline='') as l:
-    reader = csv.reader(l)
-    data = list(reader)
-
-with open(Path.home() / 'Git' /  'AlphaStonk' / 'test' / 'SPY_AAPL_BAC_PPO_portfolio.csv', newline='') as l:
-    reader2 = csv.reader(l)
-    data2 = list(reader2)
-
-with open(Path.home() / 'Git' /  'AlphaStonk' / 'test' / 'results.csv', newline='') as f:
-    reader = csv.reader(f)
-    data3 = list(reader)
+def read_csv(path):
+    with open(Path.home() / 'Desktop' / 'SPY_PPO_roi.csv', newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return data[0]
 
 def plot_roi_list(full_test_df, roi_list):
     plt.plot([round(float(i), 2) for i in roi_list], label='Bot')
     plt.plot(np.linspace(0, len(full_test_df), len(full_test_df)), [(price - full_test_df['SPY_close'].iloc[0]) / full_test_df['SPY_close'].iloc[0] * 100 for price in full_test_df['SPY_close']], label='SPY')
-    plt.plot(np.linspace(0, len(full_test_df), len(full_test_df)), [(price - full_test_df['AAPL_close'].iloc[0]) / full_test_df['AAPL_close'].iloc[0] * 100 for price in full_test_df['AAPL_close']], label='AAPL')
-    plt.plot(np.linspace(0, len(full_test_df), len(full_test_df)), [(price - full_test_df['BAC_close'].iloc[0]) / full_test_df['BAC_close'].iloc[0] * 100 for price in full_test_df['BAC_close']], label='BAC')
+    #plt.plot(np.linspace(0, len(full_test_df), len(full_test_df)), [(price - full_test_df['AAPL_close'].iloc[0]) / full_test_df['AAPL_close'].iloc[0] * 100 for price in full_test_df['AAPL_close']], label='AAPL')
+    #plt.plot(np.linspace(0, len(full_test_df), len(full_test_df)), [(price - full_test_df['BAC_close'].iloc[0]) / full_test_df['BAC_close'].iloc[0] * 100 for price in full_test_df['BAC_close']], label='BAC')
 
     plt.legend(loc=1)
     plt.show()
 
 def old_plot_roi_list(full_test_df, roi_list):
     plt.plot([round(float(i), 2) for i in roi_list], label='Bot')
-    plt.plot(np.linspace(0, len(short_df), len(short_df)), [(price - full_test_df['close'].iloc[0]) / full_test_df['close'].iloc[0] * 100 for price in full_test_df['close']], label='SPY')
+    #plt.plot(np.linspace(0, len(short_df), len(short_df)), [(price - full_test_df['close'].iloc[0]) / full_test_df['close'].iloc[0] * 100 for price in full_test_df['close']], label='SPY')
 
     plt.legend(loc=1)
     plt.show()
 
 def plot_portfolio(portfolio_list):
-    plt.plot((np.linspace(0, len(data2[0]), len(data2[0]))), [float(i) for i in portfolio_list])
+    #plt.plot((np.linspace(0, len(data2[0]), len(data2[0]))), [float(i) for i in portfolio_list])
     plt.show()
 
-'''if __name__ == "__main__":
-    test_algo(algo_path, env, roi_file_name, portfolio_file_name)'''
-#plot_roi_list(full_test_df, data[0])
-old_plot_roi_list(short_df, data3[0])
-#plot_portfolio(data2[0])
+if __name__ == "__main__":
+    test_algo(algo_path, env, roi_file_name, portfolio_file_name)
+    roi_list = read_csv(Path.home() / 'Desktop' / 'SPY_PPO_roi.csv')
+    plot_roi_list(full_test_df, data[0])
