c71a6c586278201a656acc91beffce40b1fa0974
rmalone1097 <31319374+rmalone1097@users.noreply.github.com>
2023-03-21 14:15:50 -0400

    added script for pip installs on fresh environment

---
+++ install_script.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+if __name__ == '__main__':
+    install('mplfinance')
+    install('pandas_ta')
+    install('polygon-api-client')
+    install('finnhub-python')
+    install('tensorflow_probability')
