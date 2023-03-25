4beb0fa769f3f9b840c219bafed71d61b4b5af3b
rmalone1097 <rmalone1097@gmail.com>
2023-03-25 12:29:33 -0600

    added custom callback class to ray trainer

--- trainers/ray_trainer.py
+++ trainers/ray_trainer.py
@@ -19,6 +19,11 @@ from ray import tune
 from ray.rllib.algorithms.ppo import PPO, PPOConfig
 from ray.tune.registry import get_trainable_cls
 from ray.rllib.models import ModelCatalog
+from ray.rllib.agents.callbacks import DefaultCallbacks
+from ray.rllib.env import StockEnv
+from ray.rllib.policy import Policy
+from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.evaluation import MultiAgentEpisode, RolloutWorker
 
 from policies.ray_models import *
 from envs.stock_env import StockEnv
@@ -41,7 +46,7 @@ parser.add_argument(
     "--stop-reward", type=float, default=600.0, help="Reward at which we stop training."
 )'''
 
-tickers = ['SPY', 'AAPL', 'BAC']
+tickers = ['SPY']
 cande_length = 1
 start_stamp = 928761600
 end_stamp = 1676581140
@@ -49,6 +54,37 @@ data_path = Path.home() / 'data'
 full_train_df, obs_train_df, _, _ = prepare_state_df(tickers, data_path, 2206200)
 print(full_train_df)
 print(obs_train_df)
+
+class MyCallbacks(DefaultCallbacks):
+    def on_episode_start(self, worker: RolloutWorker, base_env: StockEnv, policies: Dict[str, Policy],episode: MultiAgentEpisode, **kwargs):
+        print("episode {} started".format(episode.episode_id))
+        episode.user_data["total_roi"] = []
+        episode.hist_data["total_roi"] = []
+
+    def on_episode_step(self, worker: RolloutWorker, base_env: StockEnv, episode: MultiAgentEpisode, **kwargs):
+        total_roi = abs(base_env.total_roi)
+        episode.user_data["total_roi"].append(total_roi)
+
+    def on_episode_end(self, worker: RolloutWorker, base_env: StockEnv, policies: Dict[str, Policy], episode: MultiAgentEpisode, **kwargs):
+        total_roi = np.mean(episode.user_data["total_roi"])
+        print("episode {} ended with length {} and pole angles {}".format(episode.episode_id, episode.length, total_roi))
+        
+        episode.custom_metrics["total_roi"] = total_roi
+        episode.hist_data["total_roi"] = episode.user_data["total_roi"]
+
+    def on_sample_end(self, worker: RolloutWorker, samples: SampleBatch, **kwargs):
+        print("returned sample batch of size {}".format(samples.count))
+
+    def on_train_result(self, trainer, result: dict, **kwargs):
+        print("trainer.train() result: {} -> {} episodes".format(trainer, result["episodes_this_iter"]))
+        # you can mutate the result dict to add new fields to return
+        result["callback_ok"] = True
+
+    def on_postprocess_trajectory(self, worker: RolloutWorker, episode: MultiAgentEpisode, agent_id: str, policy_id: str, policies: Dict[str, Policy], postprocessed_batch: SampleBatch, original_batches: Dict[str, SampleBatch], **kwargs):
+        print("postprocessed {} steps".format(postprocessed_batch.count))
+        if "num_batches" not in episode.custom_metrics:
+            episode.custom_metrics["num_batches"] = 0
+        episode.custom_metrics["num_batches"] += 1
 
 if __name__ == "__main__":
 
