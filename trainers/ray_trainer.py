b1cd5e7249a29146071715f5eb750f8b8ade621b
rmalone1097 <rmalone1097@gmail.com>
2023-04-10 19:40:40 -0600

    added os cnn to ray trainer

--- trainers/ray_trainer.py
+++ trainers/ray_trainer.py
@@ -87,6 +87,7 @@ class MyCallbacks(DefaultCallbacks):
 if __name__ == "__main__":
 
     ModelCatalog.register_custom_model("simple_cnn", SimpleCNN)
+    ModelCatalog.register_custom_model("os_cnn", osCNN)
 
     ray.init(num_gpus=1)
     args = parser.parse_args()
@@ -101,7 +102,7 @@ if __name__ == "__main__":
         .resources(num_gpus=1)
         .training(
             model={
-                "custom_model": "simple_cnn"
+                "custom_model": "os_cnn"
             }
         )
     )
