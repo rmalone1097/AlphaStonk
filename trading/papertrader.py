06dba3c3dbda6a186832f8e38914cc605f54b5ef
rmalone1097 <31319374+rmalone1097@users.noreply.github.com>
2023-05-09 09:07:55 -0400

    functionality additions to papertrader

--- trading/papertrader.py
+++ trading/papertrader.py
@@ -22,8 +22,8 @@ notional_transaction_value = 10000
 # Minute difference between start stamp and end stamp
 minute_difference = 390 * 2 + 445
 
-#algo_path = Path.home() / 'ray_results'/'PPO'/'PPO_StockEnv_280fa_00000_0_2023-03-01_06-13-17'/'checkpoint_002500'
-#algo = Algorithm.from_checkpoint(algo_path)
+algo_path = Path.home() / 'ray_results'/'PPO'/'PPO_StockEnv_280fa_00000_0_2023-03-01_06-13-17'/'checkpoint_002500'
+algo = Algorithm.from_checkpoint(algo_path)
 
 def fetch_old_data(tickers):
     end_stamp = math.floor(time.time())
@@ -45,17 +45,39 @@ def check_account_value():
 if __name__ == "__main__":
     dfs = fetch_old_data(tickers)
 
-    built_dfs = []
-    for i, ticker in enumerate(tickers):
-        _, live_df = live_df_builder(tickers[i], dfs[i])
-        built_dfs.append(live_df)
-    
-    full_df, obs_df = prepare_live_df(tickers, built_dfs)
+    while True:
+        built_dfs = []
+        for i, ticker in enumerate(tickers):
+            _, live_df = live_df_builder(tickers[i], dfs[i])
+            built_dfs.append(live_df)
+        
+        full_df, obs_df = prepare_live_df(tickers, built_dfs)
 
-    env = StockEnv(config = {'full_df': full_df, 'obs_df': obs_df, 'tickers': tickers, 'print': False, 'rew_function': 'energy'})
-    obs = env.reset()
-    action = algo.compute_single_action(obs)
+        env = StockEnv(config = {'full_df': full_df, 'obs_df': obs_df, 'tickers': tickers, 'print': False, 'rew_function': 'energy'})
+        obs = env.reset()
+        action = algo.compute_single_action(obs)
 
-'''market_order_data = MarketOrderRequest(
-                    symbol = 
-)'''
+        ticker_number = max(math.floor((action - 1) / 2), 0)
+        if action == 0:
+            order_side = None
+        elif action % 2 == 1:
+            order_side = OrderSide.BUY
+        elif action % 2 == 0:
+            order_side = OrderSide.SELL
+
+        if order_side:
+            market_order_data = MarketOrderRequest(
+                                symbol = tickers[ticker_number],
+                                notional = notional_transaction_value,
+                                side = order_side,
+                                time_in_force = TimeInForce.DAY
+            )
+            market_order = trading_client.submit_order(
+                           order_data=market_order_data
+            )
+        
+        if action != action_log:
+            TradingClient.close_all_positions()
+
+        action_log = action
+        ticker_log = tickers[ticker_number]
